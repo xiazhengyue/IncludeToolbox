cb05a4fd9ef55d13786b69816f988d4eefe63a2a
Andreas Reich <r_andreas2@web.de>
2016-02-28 13:16:36 +0000

    Project setup.

---
+++ VSIncludeGraphs/IncludeGraphToolWindow.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="IncludeGraphToolWindow.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace VSIncludeGraphs
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("c87b586a-6c8b-4129-9b6d-56a761e0ac6d")]
+    public class IncludeGraphToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IncludeGraphToolWindow"/> class.
+        /// </summary>
+        public IncludeGraphToolWindow() : base(null)
+        {
+            this.Caption = "Include Graph";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new IncludeGraphToolWindowControl();
+        }
+    }
+}
