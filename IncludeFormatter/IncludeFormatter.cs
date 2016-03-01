1e3dec1599a1de0be8bffc487e93038272d28286
Andreas Reich <r_andreas2@web.de>
2016-03-01 23:33:04 +0000

    Project setup.

---
+++ IncludeFormatter/IncludeFormatter.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace IncludeFormatter
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class IncludeFormatter
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("aef3a531-8af4-4b7b-800a-e32503dfc6e2");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IncludeFormatter"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private IncludeFormatter(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static IncludeFormatter Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new IncludeFormatter(package);
+        }
+
+        private IWpfTextViewHost GetCurrentViewHost()
+        {
+            var textManager = this.ServiceProvider.GetService(typeof(SVsTextManager)) as IVsTextManager;
+
+            IVsTextView textView = null;
+            int mustHaveFocus = 1;
+            textManager.GetActiveView(mustHaveFocus, null, out textView);
+
+            var userData = textView as IVsUserData;
+            if (userData == null)
+            {
+                return null;
+            }
+            else
+            {
+                Guid guidViewHost = DefGuidList.guidIWpfTextViewHost;
+                object holder;
+                userData.GetData(ref guidViewHost, out holder);
+                var viewHost = (IWpfTextViewHost)holder;
+
+                return viewHost;
+            }
+        }
+
+        private string GetSelectedTextFromEditor(IWpfTextViewHost viewHost) => 
+            viewHost.TextView.Selection.SelectedSpans[0].GetText();
+
+        private string GetAllTextFromEditor(IWpfTextViewHost viewHost) => 
+            viewHost.TextView.TextSnapshot.GetText();
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            var viewHost = GetCurrentViewHost();
+            
+            string selectedCode = GetSelectedTextFromEditor(viewHost);
+            var selectedCodeLines = Regex.Split(selectedCode, "\r\n|\r|\n");
+
+
+            //viewHost.TextView.TextBuffer.CreateEdit();
+
+        }
+    }
+}
